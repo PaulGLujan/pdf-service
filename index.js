d67bf35e15c6302c17f8c70b9389425634292dc5
Paul Lujan <paul.lujan@smilebrands.com>
2020-07-01 16:48:13 -0700

    Service receives html file

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
 const convertHtmlToPdf = require('pdf-puppeteer');
+
+const upload = multer();
 const app = express();
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.post('/htmlToPdf', upload.single('file'), (req, res) => {
+    console.log(req.body);
+    console.log(req.file);
+    res.send('You are hitting root.');
+});
 
 app.get('/test', (req, res) => {
     res.send('You are on /test');
